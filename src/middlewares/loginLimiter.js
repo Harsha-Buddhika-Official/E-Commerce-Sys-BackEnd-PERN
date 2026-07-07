d157565316d5a6d40940bc7ee859a7b98592f65e
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2026-07-07 23:12:20 +0530

    update auth with express-rate-limit

---
+++ src/middlewares/loginLimiter.js
@@ -0,0 +1,27 @@
+// import rateLimit from 'express-rate-limit';
+
+// const loginLimiter = rateLimit({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 5, // Limit each IP to 5 login requests per `window` (here, per 15 minutes)
+//     message: 'Too many login attempts from this IP, please try again after 15 minutes',
+//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// });
+
+// export default loginLimiter;
+
+import rateLimit from "express-rate-limit";
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  handler: (req, res) => {
+    console.log("Rate limit triggered");
+    res.status(429).json({
+        success: false,
+        message: "Too many login attempts. Try again after 15 minutes."
+    });
+  },
+});
+
+export default loginLimiter;
