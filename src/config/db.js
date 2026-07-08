886b1e45fbde212299090cd58745bec93b92efe1
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2026-07-08 10:44:38 +0530

    db connection change

--- src/config/db.js
+++ src/config/db.js
@@ -2,30 +2,30 @@ import { Pool } from 'pg';
 import config from './env.js';
 
 //for local development
-const pool = new Pool({
-    user: config.db.user,
-    host: config.db.host,
-    database: config.db.name,
-    password: config.db.password,
-    port: config.db.port
-});
+// const pool = new Pool({
+//     user: config.db.user,
+//     host: config.db.host,
+//     database: config.db.name,
+//     password: config.db.password,
+//     port: config.db.port
+// });
 
 //for neon database
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 
-//   max: 10,
-//   idleTimeoutMillis: 30000,
-//   connectionTimeoutMillis: 10000,
-// });
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000,
+});
 
-// pool.on('connect', () => {
-//   console.log('✅ Database connected');
-// });
+pool.on('connect', () => {
+  console.log('✅ Database connected');
+});
 
 pool.on('error', (err) => {
   console.error('DB error:', err);
