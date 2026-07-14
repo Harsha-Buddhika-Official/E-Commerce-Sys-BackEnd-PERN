9d8103df1292edf731f178b157b7797836ab28fa
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2026-07-14 11:46:46 +0530

    Update jwt.js

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,10 +1,25 @@
+// import jwt from 'jsonwebtoken';
+// import config from '../config/env.js';
+
+// //using
+// export const generateToken = (payload) => {
+//     const expiresIn = Math.floor(Date.now() / 1000) + 60 * 60; // Login time + 1 hour
+//     return jwt.sign({ ...payload, exp: expiresIn }, config.jwtSecret);
+// }
+
+// export const verifyToken = (token) => {
+//     try {
+//         return jwt.verify(token, config.jwtSecret);
+//     } catch (err) {
+//         throw new Error('Invalid token', { cause: err });
+//     }
+// }
+
 import jwt from 'jsonwebtoken';
 import config from '../config/env.js';
 
-//using
 export const generateToken = (payload) => {
-    const expiresIn = Math.floor(Date.now() / 1000) + 60 * 60; // Login time + 1 hour
-    return jwt.sign({ ...payload, exp: expiresIn }, config.jwtSecret);
+    return jwt.sign(payload, config.jwtSecret, { expiresIn: '1h' });
 }
 
 export const verifyToken = (token) => {
