3f669ebf390454606cfbb15613fb4493a7769cfa
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2026-08-21 11:17:02 +0530

    Db change to production

--- src/config/env.js
+++ src/config/env.js
@@ -2,43 +2,43 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const requiredEnvVars = [
-    'PORT',
-    'JWT_SECRET',
+// const requiredEnvVars = [
+//     'PORT',
+//     'JWT_SECRET',
 
-    'DB_USER',
-    'DB_PASSWORD',
-    'DB_HOST',
-    'DB_NAME',
-    'DB_PORT',
+//     'DB_USER',
+//     'DB_PASSWORD',
+//     'DB_HOST',
+//     'DB_NAME',
+//     'DB_PORT',
 
-    'CLOUDINARY_CLOUD_NAME',
-    'CLOUDINARY_API_KEY',
-    'CLOUDINARY_API_SECRET'
-];
+//     'CLOUDINARY_CLOUD_NAME',
+//     'CLOUDINARY_API_KEY',
+//     'CLOUDINARY_API_SECRET'
+// ];
 
-// validate env variables
-requiredEnvVars.forEach((key) => {
+// // validate env variables
+// requiredEnvVars.forEach((key) => {
 
-    if (!process.env[key]) {
-        throw new Error(`${key} is required in .env`);
-    }
+//     if (!process.env[key]) {
+//         throw new Error(`${key} is required in .env`);
+//     }
 
-});
+// });
 
 const config = {
 
     port: process.env.PORT,
 
     jwtSecret: process.env.JWT_SECRET,
-    // databaseUrl: process.env.DATABASE_URL,
-    db: {
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        name: process.env.DB_NAME,
-        port: process.env.DB_PORT
-    },
+    databaseUrl: process.env.DATABASE_URL,
+    // db: {
+    //     user: process.env.DB_USER,
+    //     password: process.env.DB_PASSWORD,
+    //     host: process.env.DB_HOST,
+    //     name: process.env.DB_NAME,
+    //     port: process.env.DB_PORT
+    // },
 
     cloudinary: {
         cloudName: process.env.CLOUDINARY_CLOUD_NAME,
